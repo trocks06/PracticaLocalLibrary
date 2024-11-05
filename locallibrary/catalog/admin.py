72cfc6e5d5e24fd4b9d44ece3238c571dc15665b
trocks06 <denishatkov17@gmail.com>
2024-11-05 12:48:22 +0700

    Add file

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -3,12 +3,13 @@ from .models import *
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
 
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('status', 'book')
 
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author, AuthorAdmin)
